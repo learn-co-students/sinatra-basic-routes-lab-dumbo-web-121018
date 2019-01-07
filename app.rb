51d8fb7c34c21667e2f2cd8e2e305047666c6e2d
horangijk <iamjooyoung@gmail.com>
2019-01-07 11:00:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Jerold Kim."
+  end
+
+  get '/hometown' do
+    "My hometown is Woodside, NY."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Can't Take My Eyes Off of You' by Muse."
+  end
+
 end
