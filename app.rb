44ee9ae3f18290c08b9e57cbe390605520a50637
Tara Sabella <tarasabella@gmail.com>
2019-01-07 15:46:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+  get '/name' do
+    "My name is @name"
+  # @name = Name.all
+  # erb :'name/index.html.erb'
+  end
+  
+  get '/hometown' do
+    "My hometown is @hometown"
+  # @hometown = Hometown.all 
+ 
+  # erb :'hometown/index.html.erb'
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is @favorite-song"
+  # @name = FavoriteSong.all 
+  # erb :'favorite-song/index.html.erb'
+  end
+end 
